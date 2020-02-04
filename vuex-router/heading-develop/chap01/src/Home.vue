16566476738c4da7678019ac8f988499b97ed627
PSK <wiv821253@gmail.com>
2020-02-04 23:25:45 +0900

    add Home Component import

---
+++ vuex-router/heading-develop/chap01/src/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{ homeTitle }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        data() {
+            return {
+                homeTitle: "홓홓홓"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
