d7d114194247732b4abbc52acdb9a50609c9af9c
PSK <wiv821253@gmail.com>
2020-02-05 23:49:15 +0900

    add default frame

---
+++ vuex-router/heading-develop/chap02/src/components/UserComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="blue lighten-3 pa-3">
+        <h1>User Component</h1>
+        <p>Name: Vue-js</p>
+        <hr>
+        <v-layout row wrap>
+            <v-flex xs12 sm6>
+                <UserDetail></UserDetail>
+            </v-flex>
+            <v-flex xs12 sm6>
+                <UserEdit></UserEdit>
+            </v-flex>
+        </v-layout>
+    </div>
+</template>
+
+<script>
+    import UserEdit from "./UserEdit";
+    import UserDetail from "./UserDetail";
+
+    export default {
+        name: "UserComponent",
+        components: {
+            UserDetail, UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
