a330097afcc4a71ad2806097d5c2d04f4a5eecad
PSK <wiv821253@gmail.com>
2020-02-06 00:01:14 +0900

    add props

--- vuex-router/heading-develop/chap02/src/components/UserDetail.vue
+++ vuex-router/heading-develop/chap02/src/components/UserDetail.vue
@@ -1,10 +1,19 @@
 <template>
-    
+    <div class="red lighten-3 pa-3">
+        <h3>회원정보 확인</h3>
+        <p>{{ nameOfChild }}</p>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "UserDetail"
+        name: "UserDetail",
+        props: ['nameOfChild'],
+        data() {
+            return {
+
+            }
+        }
     }
 </script>
 
