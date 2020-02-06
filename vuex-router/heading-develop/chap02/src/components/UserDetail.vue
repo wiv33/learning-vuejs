bca89f23c551a9aef4fa04fb86002f4ae28edf01
PSK <wiv821253@gmail.com>
2020-02-06 23:19:10 +0900

    ready emit

--- vuex-router/heading-develop/chap02/src/components/UserDetail.vue
+++ vuex-router/heading-develop/chap02/src/components/UserDetail.vue
@@ -3,6 +3,26 @@
         <h3>회원정보 확인</h3>
         <p>{{ nameOfChild }}</p>
         <p>{{ helloWorld }}</p>
+        <v-btn @click="switchName()">Change Name!!</v-btn>
+
+        <v-list dense>
+            <v-list-item>
+                <v-list-item-content>이름:</v-list-item-content>
+                <v-list-item-content class="align-end">{{ name }}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+                <v-list-item-content>이름:</v-list-item-content>
+                <v-list-item-content class="align-end">{{ address }}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+                <v-list-item-content>전화번호:</v-list-item-content>
+                <v-list-item-content class="align-end">{{ phone }}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+                <v-list-item-content>반려견 유무:</v-list-item-content>
+                <v-list-item-content class="align-end">{{ hasDogFunc }}</v-list-item-content>
+            </v-list-item>
+        </v-list>
     </div>
 </template>
 
@@ -10,21 +30,21 @@
     export default {
         name: "UserDetail",
         /* parent 가 보내준 name 을 nameOfChild 로 받는다. */
-        props: {
-            nameOfChild: {
-                type: String,
-                // required: true,
-                default: "PROTOCOL"
-            }
-        },
+        props: ['name', 'address', 'phone', 'hasDog', 'nameOfChild'],
         data() {
-            return {
-
-            }
+            return {}
         },
         computed: {
             helloWorld() {
                 return this.nameOfChild + " plus";
+            },
+            hasDogFunc() {
+                return this.hasDog ? "있음" : "없음"
+            }
+        },
+        methods: {
+            switchName() {
+                this.nameOfChild = "switch";
             }
         }
     }
