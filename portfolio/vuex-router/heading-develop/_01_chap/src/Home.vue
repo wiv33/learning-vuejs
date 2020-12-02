1de2f64585611ba187731472875e352985782e66
PSK <40161102+wiv33@users.noreply.github.com>
2020-12-02 20:30:49 +0900

    update

---
+++ portfolio/vuex-router/heading-develop/_01_chap/src/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <app-status></app-status>
+        <h1>{{ homeTitle }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        data() {
+            return {
+                homeTitle: "홓홓홓"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ portfolio/vuex-router/heading-develop/_01_chap/src/StatusVue.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2> {{ title }}</h2>
+</template>
+
+<script>
+    export default {
+        name: "StatusVue",
+        data() {
+            return {
+                title: "상태 좋습니다."
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
