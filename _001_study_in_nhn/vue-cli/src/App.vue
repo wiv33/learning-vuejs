dffb4d2f263543ddb6223c6dc10ee1282430e9cf
PSK <psawesome@nhnsoft.com>
2021-09-03 10:18:42 +0900

    add vue study - 4 vue cli

---
+++ _001_study_in_nhn/vue-cli/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ str }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      str : 'hi'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
